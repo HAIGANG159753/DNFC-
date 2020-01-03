c61477391d8354146864418127b9f684b4b8e56d
HAIGANG <771262509@qq.com>
2020-01-03 19:27:01 +0800

    01031926

--- Test.cpp
+++ Test.cpp
@@ -6,7 +6,20 @@ int main() {
 	cout << "哈哈哈哈哈哈哈哈哈";
 	cout << endl;
 	cout << "1232342342342342" << endl;
-	return 13398;
+	int carrots = 25;
+	cout << carrots;
+	cout << "carrots. ";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "I Have " << carrots << " carrots." << endl;
+
+	cout << "请输入你想要的数量："<<endl;
+	cin >> carrots;
+	cout << "加上2个以后 ";
+	carrots = carrots + 2;
+
+	cout << "现在的数量 " << carrots << " carrots." << endl;
+	return (0);
 
 
 
